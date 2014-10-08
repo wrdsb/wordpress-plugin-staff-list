8a34e55a640614a75b5b5fff93da828b0096cc52
James Schumann <jschumann@gmail.com>
2014-10-08 09:44:59 -0400

    prototype table in place; about to convert to WP native

--- admin-pages/options.php
+++ admin-pages/options.php
@@ -1,13 +1,26 @@
 <?php
 $users = get_users();
 
-// Array of WP_User objects.
-foreach ( $users as $user ) {
-  echo '<p>';
-  echo esc_html($user->user_email);
-  echo '<pre>';
-  print_r(get_user_meta($user->ID));
-  echo '</pre>';
-  echo '</p>';
-}
+// Table of users, with options.
+echo '<table border="1" width="100%">';
+  echo '<tr>';
+    echo '<th>Display?</th>';
+    echo '<th>Last Name</th>';
+    echo '<th>First Name</th>';
+    echo '<th>Job Description</th>';
+    echo '<th>Email</th>';
+    echo '<th>Voicemail</th>';
+  echo '</tr>';
+  //TODO We should figure out how to grab all the meta fields with one query
+  foreach ( $users as $user ) {
+    echo '<tr>';
+      echo '<td>' . get_user_meta($user->ID, 'in_staff_list', true)   . '</td>';
+      echo '<td>' . get_user_meta($user->ID, 'last_name', true)       . '</td>';
+      echo '<td>' . get_user_meta($user->ID, 'first_name', true)      . '</td>';
+      echo '<td>' . get_user_meta($user->ID, 'job_description', true) . '</td>';
+      echo '<td>' . esc_html($user->user_email)                       . '</td>';
+      echo '<td>' . get_user_meta($user->ID, 'voicemail', true)       . '</td>';
+    echo '</tr>';
+  }
+echo '</table>';
 ?>
